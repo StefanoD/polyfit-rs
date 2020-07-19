9d0dcee92b7249cb2c7b73a4d498ca677e0564d7
Stefano Di Martino <stefano.d@posteo.de>
2020-07-19 21:39:49 +0200

    Extend test

--- tests/lib.rs
+++ tests/lib.rs
@@ -5,7 +5,7 @@ mod tests {
     use polyfit_rs as poly;
 
     #[test]
-    fn it_works() {
+    fn simple_parabola_fitting() {
         // f(x) = a*x² + b * x + c
         // c = 1.0, b = 1.1, a = 2.0
         // Order of monomials increases with the vector index
@@ -13,7 +13,7 @@ mod tests {
         let mut y_values: Vec<f64> = Vec::with_capacity(21);
         let mut x_values: Vec<f64> = Vec::with_capacity(21);
 
-        for i in (-10..11)
+        for i in -10..11
         {
             let x = i as f64;
             let y: f64 = parameters[0] + parameters[1] * x + parameters[2] * x * x;
@@ -22,6 +22,14 @@ mod tests {
         }
 
         let parameters = poly::polyfit_rs::polyfit(&x_values, &y_values, 2);
-        let bla = 1;
+        let epsilon = 0.0001;
+        let mut i = 0;
+        for x in x_values
+        {
+            let fitted_y: f64 = parameters[0] + parameters[1] * x + parameters[2] * x * x;
+            let error = (fitted_y - y_values[i]).abs();
+            assert!(error <= epsilon);
+            i += 1;
+        }
     }
 }

3d90880d0c065ebae1c9eb5a093d930873fa5961
Stefano Di Martino <stefano.d@posteo.de>
2020-07-30 21:50:00 +0200

    remove plot from test

--- tests/lib.rs
+++ tests/lib.rs
@@ -4,8 +4,6 @@ extern crate rand;
 mod tests {
     use rand::{Rng, SeedableRng};
     use polyfit_rs as poly;
-    use std::io::prelude::*;
-    use std::fs::File;
 
     #[test]
     fn simple_parabola_fitting() {
@@ -20,24 +18,17 @@ mod tests {
         let mut rng = rand::rngs::StdRng::seed_from_u64(5000u64);
         let epsilon = 10.0f64;
 
-        let mut buffer = File::create("signal.csv").ok().unwrap();
-
         for i in -100..110
         {
             let x = (i as f64) / 10.0;
             let y: f64 = parameters[0] + parameters[1] * x + parameters[2] * x * x;
             let error: f64 = rng.gen_range(-epsilon, epsilon);
             let y_with_error = y + error;
-            y_values_with_errors.push(y + error);
+            y_values_with_errors.push(y_with_error);
             x_values.push(x);
             y_values.push(y);
-            buffer.write(x.to_string().as_bytes());
-            buffer.write(b";");
-            buffer.write(y_with_error.to_string().as_bytes());
-            buffer.write(b"\n");
         }
 
-        buffer = File::create("fitted_signal.csv").ok().unwrap();
         let fitted_parameters = poly::polyfit_rs::polyfit(&x_values, &y_values_with_errors, 2).unwrap();
         
         let mut i = 0;
@@ -46,10 +37,6 @@ mod tests {
             let fitted_y: f64 = fitted_parameters[0] + fitted_parameters[1] * x + fitted_parameters[2] * x * x;
             let error = (fitted_y - y_values[i]).abs();
             assert!(error <= epsilon);
-            buffer.write(x.to_string().as_bytes());
-            buffer.write(b";");
-            buffer.write(fitted_y.to_string().as_bytes());
-            buffer.write(b"\n");
             i += 1;
         }
     }

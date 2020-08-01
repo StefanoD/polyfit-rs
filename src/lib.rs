6a06786081857a1964b254753419ad27a04819cf
philippeitis <33013301+philippeitis@users.noreply.github.com>
2020-07-31 17:16:00 -0700

    Simplify return, use enumeration, formatting

--- src/lib.rs
+++ src/lib.rs
@@ -6,20 +6,20 @@ pub mod polyfit_rs {
     /// @param y_values The y-values
     /// @param polynomial_degree The degree of the polynomial. I. e. 2 for a parabola.
     /// @return Degree of monomials increases with the vector index
-    pub fn polyfit<T: na::RealField>(x_values : &[T], y_values : &[T], polynomial_degree: usize) -> Result<Vec<T>, &'static str>
-    {
+    pub fn polyfit<T: na::RealField>(
+        x_values: &[T],
+        y_values: &[T],
+        polynomial_degree: usize,
+    ) -> Result<Vec<T>, &'static str> {
         let number_of_columns = polynomial_degree + 1;
         let number_of_rows = x_values.len();
         let mut a = na::DMatrix::zeros(number_of_rows, number_of_columns);
 
-        for row in 0..number_of_rows 
-        {
+        for (row, &x) in x_values.iter().enumerate() {
             // First column is always 1
             a[(row, 0)] = T::one();
-            let x = x_values[row];
-    
-            for col in 1..number_of_columns
-            {
+
+            for col in 1..number_of_columns {
                 a[(row, col)] = x.powf(na::convert(col as f64));
             }
         }
@@ -27,13 +27,10 @@ pub mod polyfit_rs {
         let b = na::DVector::from_row_slice(y_values);
 
         let decomp = na::SVD::new(a, true, true);
-        let x = decomp.solve(&b, na::convert(0.000000000000000001));
 
-        let x = match x {
+        match decomp.solve(&b, na::convert(1e-18f64)) {
             Ok(mat) => Ok(mat.data.into()),
             Err(error) => Err(error),
-        };
-        
-        return x;
+        }
     }
 }
